c97cd25467046048a7a7a4b4a36a7260801e8501
Akshatmishra1211 <raghav.75856@gmail.com>
2025-07-17 16:19:23 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:5000";
+export const server = "https://shiksha-backend-h51x.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(     // connect react app with the html in index.html
   <React.StrictMode>
